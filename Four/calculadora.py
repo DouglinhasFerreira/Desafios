25628ea810f4268d7499da1a8b570c3b54cbed82
DouglinhasFerreira <douglinhasprogdev@gmail.com>
2024-09-07 22:49:31 -0300

    Calculadora-PT1

---
+++ Four/calculadora.py
@@ -0,0 +1,21 @@
+def adicao(x,y):
+    return x+y
+
+def subtracao(x,y):
+    return x-y
+
+def multiplicacao(x,y):
+    return x*y
+
+def divisao(x,y):
+    return x/y
+
+def calculadora():
+    print ("** Bem-vindo à Calculadora **")
+    print (" Selecione a operação que deseja ")
+    print ("1. Adicao")
+    print ("2. Subtracao")
+    print ("3. Multiplicacao")
+    print ("4. Divisao")
+
+escolha = input("Digite sua escolha (1/2/3/4): ")
