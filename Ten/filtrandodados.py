41ae0c2bd2c581948497065f98a88e62ae33fd5c
DouglinhasFerreira <douglinhasprogdev@gmail.com>
2024-09-13 23:11:15 -0300

    media

---
+++ Ten/filtrandodados.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+data = {
+    'nome': ['Ana', 'João', 'Maria', 'Pedro', 'Lucas', 'Carla'],
+    'departamento': ['TI', 'RH', 'TI', 'Financeiro', 'TI', 'RH'],
+    'salario': [5000, 4500, 5200, 6000, 4800, 4700]
+} 
+
+df = pd.DataFrame(data)
+funcionarios_ti = df[df['departamento'] == 'TI']
+print(funcionarios_ti)
+
+media_salarial = funcionarios_ti['salario'].mean()
+
+print(f"Media salarial dos funcionarios de TI:{media_salarial}")
